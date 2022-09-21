d6d9407e4ff43f44eca151f3a021e794e78afb99
Jansel Roa <65916518+janselroa@users.noreply.github.com>
2022-09-21 17:10:40 -0400

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-mongoose.connect(`mongodb+srv://janselroa:Gi4QtWjsRM1JmdPN@cluster0.mkb7tew.mongodb.net/midb?retryWrites=true&w=majority`,{
+mongoose.connect(process.env.DBHOST,{
 	 useNewUrlParser: true,
 	 useUnifiedTopology: true
 }).then(data=>console.log("DB conectada")).catch(err=>console.error(err))
